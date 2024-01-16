f9160bdaada3f8ea60cbc1c7df7cf20f90225f1e
Markus Wolff <markus.wolff@arqsoft.net>
2024-01-16 15:26:20 +0100

    REL 3.2.0

--- src/Net.Arqsoft.QsMapper/Properties/AssemblyInfo.cs
+++ src/Net.Arqsoft.QsMapper/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("arQSoft")]
 [assembly: AssemblyProduct("Net.Arqsoft.QsMapper")]
-[assembly: AssemblyCopyright("Copyright © arQSoft UG 2012 - 2017")]
+[assembly: AssemblyCopyright("Copyright © arQSoft UG 2012 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
