06d713dc24b7d04c4affa8bfc0317202fab74000
Markus Wolff <markus.wolff@arqsoft.net>
2025-05-06 20:16:58 +0200

    REL 4.1.1

--- src/Net.Arqsoft.QsMapper.NetCore/CommandBuilder/TableValueParameter.cs
+++ src/Net.Arqsoft.QsMapper.NetCore/CommandBuilder/TableValueParameter.cs
@@ -2,7 +2,7 @@
 
 public class TableValueParameter
 {
-    public string TypeName { get; set; }
-    public IDictionary<string, Type> Columns { get; set; }
-    public List<List<object>> Rows { get; set; }
+    public string TypeName { get; set; } = "System.Array";
+    public IDictionary<string, Type> Columns { get; set; } = new Dictionary<string, Type> { { "Id", typeof(int) } };
+    public List<List<object>> Rows { get; set; } = new();
 }
